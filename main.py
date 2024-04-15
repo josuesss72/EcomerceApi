eaf8fae514c890bdd565f981b4f5b8ba0a8b9e88
josuesss72 <jjdanielsarmiento@gmail.com>
2024-04-15 17:38:22 -0500

    JOSUE => agrege el middlewares CORS para permitir solicitudes desde un orgigen

--- main.py
+++ main.py
@@ -3,7 +3,24 @@ from router.user_router import user
 from router.product_router import product
 from router.cart_router import cart
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Para resolver el problema de CORS (Cross-Origin Resource Sharing)
+# para permitir solicitudes
+origins = [
+    "http://localhost:5173"
+]
+
+# ADD MIDDLEWARES
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user)
 app.include_router(product)
