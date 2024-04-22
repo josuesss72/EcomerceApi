2dc7e895c36b56504d5f1fd0460e3281a54f1a32
josuesss72 <jjdanielsarmiento@gmail.com>
2024-04-22 18:12:01 -0500

    JOSUE => configure las cors que cualquier origen sea permitido

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ origins = [
 # ADD MIDDLEWARES
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
