b1883712e0e03e807bd407fa5389bda51145e51c
josuesss72 <jjdanielsarmiento@gmail.com>
2024-04-22 19:07:26 -0500

    JOSUE => configuracion de cors

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ origins = [
 # ADD MIDDLEWARES
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
